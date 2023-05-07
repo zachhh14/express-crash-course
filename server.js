d0af0dee22de7d4aecd260610754b49f94cc26a8
zachhh14 <zactec115@gmail.com>
2023-05-07 14:41:18 +0800

    EXPRESS CRASH COURSE
    - server setup
    - basic routing
    - sending data
    - rendering HTML
    - routers
    - advanced routing
    - middleware
    - rendering static files
    - parsing form/JSON data
    - parse query params

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/',(req, res)=>{
-    console.log("here")
-    res.send('hi')
-})
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
-app.listen(3000)
+app.set("view engine", "ejs");
+
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
+
+app.listen(3000);
